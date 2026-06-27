2ed84f7ddfda18ed1850a792d40815e224f0a901
abdulhadipoonoor <abdulhadipoonoor786@gmail.com>
2026-06-27 08:19:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector("button").addEventListener("click", function(){
   window.location.href = "#";
 });
+function toggleDarkMode(){
+  document.body.classList.toggle("dark");
+}
+document.getElementById("searchBox").addEventListener("keyup", function(){
+  let value = this.value.toLowerCase();
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let text = card.innerText.toLowerCase();
+    card.style.display = text.includes(value) ? "block" : "none";
+  });
+});
